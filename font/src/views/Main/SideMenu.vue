479489600e2ee08e6852dda94ad743c95d9b9a3d
徐佳慧 <2649387037@qq.com>
2018-04-08 11:46:15 +0800

    管理员首页修改

--- font/src/views/Main/SideMenu.vue
+++ font/src/views/Main/SideMenu.vue
@@ -1,17 +1,23 @@
 <template  id='sideMenu'>
     <div>
         <template v-for="item in routes[0].children" >
-        <el-submenu :index="item.path"  :key='item.name' v-if='item.children'>
-         <template slot="title"> <i :class="item.icon"></i>{{item.cn}}</template>
-         
-            <template v-for='child in item.children' >
-                 <el-menu-item :index="child.path" :key='child.name'>{{child.cn}}</el-menu-item>
+            <el-submenu :index="item.path"  :key='item.name' v-if='item.children'>
+                <template slot="title"> <i :class="item.icon"></i>{{item.cn}}</template>
+                <template v-for='child in item.children' >
+                    <el-submenu :index="child.path" :key='child.name' v-if='child.children'>
+                        <template slot="title"><i ></i>{{child.cn}}</template>
+                        <template v-for='child in child.children' >
+                            <el-menu-item :index="child.path" :key='child.name'>{{child.cn}}</el-menu-item>
+                        </template>
+                    </el-submenu>                    
+                    <el-menu-item :index="child.path" :key='child.name' v-if='!child.children'><i></i><span slot="title">{{child.cn}}</span> </el-menu-item>
+                    
                 </template>  
-        </el-submenu>
-        <el-menu-item :index="item.path"  :key='item.name' v-if='!item.children'><i :class="item.icon"></i><span slot="title">{{item.cn}}</span> </el-menu-item>
- </template>
-    </div>
-    
+            </el-submenu>
+            <el-menu-item :index="item.path"  :key='item.name' v-if='!item.children'><i :class="item.icon"></i><span slot="title">{{item.cn}}</span> </el-menu-item>
+            
+        </template>
+    </div> 
 </template>
 <script>
 export default {
@@ -27,7 +33,7 @@ export default {
         }
 
     },
-        mounted(){
+    mounted(){
         console.log('sdfs',this.routes[0])
     }
 }
