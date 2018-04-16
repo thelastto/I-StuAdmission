757cd1463cbf3e8035cc18b654fde6a075c9c83f
徐佳慧 <2649387037@qq.com>
2018-04-16 21:07:38 +0800

    修改eleme样式

--- font/src/App.vue
+++ font/src/App.vue
@@ -20,5 +20,21 @@ export default {
  li{
  outline: 0px!important;
  }
-    
+.el-pagination__editor {
+    border: none;
+}
+.el-pager li.active {
+    color: #fff;
+}
+.el-select-dropdown__item.selected {
+    color: #fff;
+}
+.el-select .el-input .el-input__icon {
+    margin-top: -50%;
+}
+.el-table .cell {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap
+}
 </style>
