08c50c2cb6aa847bcb7f015762e30183adccf0ae
徐佳慧 <2649387037@qq.com>
2018-04-16 21:11:50 +0800

    contactUs页面

---
+++ font/src/views/StuWebSite/AboutUs/contactUs.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id = "contactUs">
+    <router-view></router-view> 
+  </div>
+</template>
+<script>
+export default {
+  data(){
+      return{}
+  }
+}
+</script>
+<style scoped>
+
+</style>
