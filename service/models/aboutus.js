6848b7754df4df53b02116d32cf417ab7913fba6
徐佳慧 <2649387037@qq.com>
2018-04-16 21:12:25 +0800

    修改aboutus Model

--- service/models/aboutus.js
+++ service/models/aboutus.js
@@ -1,13 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
 const AboutusSchema = new Schema({
-  aboutSCIE: {
+  name: {
     type: String
   },
-  contactUs: {
+  content : {
     type: String
-  }
+  },
+  createBy : {
+    type: String
+  },
+  createDate :{
+    type: Date,
+    require: true,
+    default: Date.now
+  },
+  lastEditBy : {
+    type: String
+  },
+  lastEditDate: {
+    type: Date,
+    require: true,
+    default: Date.now 
+  },
+
+  
 });
 
 module.exports = mongoose.model('Aboutus', AboutusSchema);
