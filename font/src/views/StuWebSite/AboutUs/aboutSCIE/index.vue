91d36dc05d55bd8807bfb949f7081383da002c6d
徐佳慧 <2649387037@qq.com>
2018-04-16 21:15:34 +0800

    aboutSCIE默认页面

---
+++ font/src/views/StuWebSite/AboutUs/aboutSCIE/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id = "aboutSCIE">
+    <router-view>
+    </router-view> 
+  </div>
+</template>
+<script>
+export default {
+  data(){
+      return{}
+  }
+}
+</script>
+<style scoped>
+
+</style>
