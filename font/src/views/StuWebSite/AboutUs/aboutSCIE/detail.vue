488ed1da4f8ce65f4989a8e2718055909e0bb3de
徐佳慧 <2649387037@qq.com>
2018-04-16 21:15:09 +0800

    detail页面

---
+++ font/src/views/StuWebSite/AboutUs/aboutSCIE/detail.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id = "aboutSCIEDetail">
+    <div v-html="content"></div>
+  </div>
+</template>
+<script>
+import { getAboutSCIEDetail } from 'api/api';
+export default {
+  data(){
+      return{
+          id: this.$route.params.id,
+          content:''
+      }
+  },
+  mounted(){
+      let that = this;
+      getAboutSCIEDetail({id:that.id}).then(res => {
+          console.log(res.data);
+          if (!res.data.code) {
+            that.content = res.data.content;
+          } else {
+              this.$message({
+                type: 'error',
+                message: res.data.message
+              })
+          } 
+      })
+  }
+}
+</script>
+<style scoped>
+
+</style>
