c1f232ed6972098a5e8c9e4ca84a106368c5ffce
徐佳慧 <2649387037@qq.com>
2018-04-19 15:08:57 +0800

    新增student api的路径

--- service/routes/index.js
+++ service/routes/index.js
@@ -6,4 +6,5 @@ module.exports = (app) => {
   app.use('/api/feedback', require('./feedback')); 
   app.use('/oauth', require('./oauth')); 
   app.use('/api/aboutUs', require('./aboutUs')); 
+  app.use('/api/student', require('./student')); 
 };
