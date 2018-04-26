18d3dc61c176f3b1eb2bd85a4972eb73ee8ce656
xujiahui <2649387037@qq.com>
2018-04-26 23:03:57 +0800

    新增中介详情页面

---
+++ font/src/views/Channel/agency/detail.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id = "stuDetail">
+    <div style="width:500px;">
+        <el-form ref="form" :model="form" label-width="80px" size="mini">
+          <el-form-item prop="sNumber" label="学号：" >
+              <span>{{form.sNumber}}</span>
+          </el-form-item>  
+          <el-form-item prop="name" label="姓名：">
+              <span>{{form.name}}</span>
+          </el-form-item>
+          <el-form-item prop="major" label="专业：">
+              <span>{{form.major}}</span>
+          </el-form-item>
+          <el-form-item prop="email" label="邮箱：">
+              <span>{{form.email}}</span>
+          </el-form-item>
+          <el-form-item prop="notes" label="备注：">
+              <span>{{form.notes}}</span>
+          </el-form-item>
+        </el-form>
+    </div>
+  </div>
+</template>
+<script>
+import { getStuDetail } from 'api/api';
+export default {
+  data(){
+      return{
+          id: this.$route.params.id,
+          form:{
+              sNumber:'',
+              name:'',
+              major:'',
+              email:'',
+              notes:''
+          }
+      }
+  },
+  mounted(){
+      let that = this;
+      getStuDetail({id:that.id}).then(res => {
+          console.log(res.data);
+          if (!res.data.code) {
+            that.form = res.data.stu;
+          } else {
+              this.$message({
+                type: 'error',
+                message: res.data.message
+              })
+          } 
+      })
+  }
+}
+</script>
+<style scoped>
+
+</style>
