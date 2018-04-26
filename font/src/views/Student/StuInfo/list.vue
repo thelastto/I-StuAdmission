c127cb927a381ae63003e513d1df141a93c39c81
xujiahui <2649387037@qq.com>
2018-04-26 22:56:39 +0800

    留学生列表页面新增：导入，导出，批量删除

--- font/src/views/Student/StuInfo/list.vue
+++ font/src/views/Student/StuInfo/list.vue
@@ -1,18 +1,44 @@
 <template>
+
     <div >
+        
         <div style="margin-bottom:20px;"> 
           <el-tooltip content="添加一个新的学生信息" placement="top">
           <el-button type="primary" plain icon="el-icon-plus" 
             @click="handleAdd()">新建</el-button>
           </el-tooltip>
-          <el-tooltip content="从本地exel导入" placement="top">
-            <el-button type="success" plain icon="el-icon-document" >导入</el-button>
-          </el-tooltip>
-          <el-tooltip content="导出到本地" placement="top">
-            <el-button type="success" plain icon="el-icon-share" >导出</el-button>
-          </el-tooltip>
+          <el-button  type="danger" plain icon="el-icon-delete" @click="handleBatchDelete">批量删除</el-button>
+          <el-button type="success" plain icon="el-icon-document"  
+              @click="dialogFormVisible = true">导入</el-button>
+
+          <el-dialog title="导入" :visible.sync="dialogFormVisible">
+
+            <el-upload class="upload-demo" ref="upload" :on-preview="handlePreview"
+              action="/api/student/saveStuFromFile" multiple 
+              :on-remove="handleRemove" :file-list="fileList" :auto-upload="false"
+              :on-success="handleSuccess"
+              accept="application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet">
+              <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+              <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
+              <div slot="tip" class="el-upload__tip">
+                  注意事项：<br>
+                  1.您上传的表格必须严格按照模板的格式填写，其中<span style="color:red">标红的为必填项</span><br>
+                  2.不按照格式填写或没有填必填项会导致错误<br>
+                  3.若您导入的学生已经存在，则会<span style="color:red">覆盖</span>其之前的资料，导入前请仔细核对<br>
+                  4.若您还未下载模板，请先<a href="/api/download?filename=stuInfoTemplate">下载模板</a><br>
+                  5.只能上传xlsx、xls格式的文件
+              </div>
+            </el-upload>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogFormVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+            </div>
+          </el-dialog>
           
-          <el-button  type="danger" plain icon="el-icon-delete" >批量删除</el-button>
+
+          <a href="/api/student/export" style="display:inline-block;padding-left:10px">
+                <el-button type="success" plain icon="el-icon-document">导出</el-button>
+          </a>
 
           <el-form :inline="true"
                      :model="formInline"
@@ -33,18 +59,18 @@
             <div class="feedback__table">
                 <el-table :data="stuList"
                           :v-loading="loading"
-                          height="641"
                           stripe
                           border
                           style="width: 100%"
-                          :default-sort="{prop:'id',order:'descending'}">
+                          :default-sort="{prop:'id'}"
+                          @selection-change="changeFun">
                     <el-table-column type="selection"
                                      sortable
-                                    >
+                                     @selection-change="changeFun">
                     </el-table-column>
                     <el-table-column label="学号"
                                      prop='id'
-                                     min-width="100">
+                                     min-width="120">
                         <template slot-scope="scope">
                             <el-icon name="id"></el-icon>
                             <span style="margin-left: 10px">{{ scope.row.sNumber }}</span>
@@ -82,26 +108,30 @@
     
             </div>
         </div>
-        <div class="feedback__pagination">
+        <div >
             <div class="block">
                 <el-pagination
-      @current-change="handleCurrentChange"
-      :current-page="currentPage"
-      :page-sizes="[10, 20, 30, 40]"
-      :page-size="10"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="page.total">
-    </el-pagination>
+                  @current-change="handleCurrentChange"
+                  @size-change = "handleSizeChange"
+                  :current-page="currentPage"
+                  :page-sizes="[10, 20, 30, 40]"
+                  :page-size="10"
+                  layout="total, sizes, prev, pager, next, jumper"
+                  :total="page.total">
+                </el-pagination>
             </div>
         </div>
+        
     </div>
 </template>
 <script>
-import { getStuList, removeStu } from 'api/api';
+import { getStuList, removeStu, download , exportToExel,batchDeleteStu } from 'api/api';
 import 'common/index';
 export default {
     data() {
         return {
+            dialogFormVisible: false,
+            fileList: [],
             formInline: {
                 name: '',
                 keywords: '',
@@ -120,8 +150,9 @@ export default {
             },
             page: {
                 total: 0,
-                sizes: [15, 20, 25, 30],
-            }
+                sizes: [10, 20, 30, 40],
+            },
+            multipleSelection:[]
         }
     },
     mounted() {
@@ -184,20 +215,76 @@ export default {
                     });
                 })
             });
-      }
+      },
+      handleExport(){
+          exportToExel().then(res => {
+                    console.log(res)
+                }).catch(() => {
+                    console.log(res)
+                   
+            })
+      },
+      submitUpload() {
+        this.$refs.upload.submit();
+      },
+      handleRemove(file, fileList) {
+        console.log(file, fileList);
+      },
+      handlePreview(file) {
+        console.log(file);
+      },
+      handleSuccess(response, file, fileList){
+          if(!response.code){
+              this.$message({
+                type: 'success',
+                message: '导入成功!'
+              });
+              this.getData();
+          }else{
+              this.$message({
+                type: 'error',
+                duration:0,
+                showClose:true,
+                message: '数据'+JSON.stringify(response.err)+'错误，已停止导入'
+              });
+          }
+      },
+      handleBatchDelete(){
+          let that = this;
+          this.$confirm('确定要删除所选留学生吗?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(() => {
+                batchDeleteStu({stuList:that.multipleSelection}).then(res =>{
+                    if(!res.code){
+                        that.$message({
+                          type: 'success',
+                          message: '删除成功'
+                        })
+                        that.getData();
+                    }else{
+                        that.$message({
+                          type: 'error',
+                          message: res.message
+                        });
+                    }
+              
+                })
+            })
+      },
+      changeFun(val) {
+        this.multipleSelection = val;
+      },
     }
 }
 </script>
-<style lang="scss" scoped>
+<style>
+.el-dialog{
+    margin: 0 0 0 0;
+}
 .form-inline{
     float:right;
 
-}
-.feedback {
-    &__pagination {
-        padding-top: 30px;
-        width: 800px;
-        margin: 0 auto;
-    }
 }
 </style>
