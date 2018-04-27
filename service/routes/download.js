f1634260c7c0aad5973ab16e5e3e5571af733722
xujiahui <2649387037@qq.com>
2018-04-28 00:32:42 +0800

    新增中介模板下载

--- service/routes/download.js
+++ service/routes/download.js
@@ -7,9 +7,10 @@ var multipartMiddleware = multipart();
 
 router.get('/', multipartMiddleware, function (req, res) {  
     if (req.query.filename=='stuInfoTemplate') {  
-    
       //相对路径  
       res.download('public/downloads/留学生信息模板.xlsx'); 
+    }else if(req.query.filename=='agencyTemplate'){
+      res.download('public/downloads/中介模板.xlsx'); 
     }else{  
         res.json({code:1,message:'找不到请求文件'});
     }  
