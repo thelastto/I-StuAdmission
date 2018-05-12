584aa4483bf354ac2fecc8f16fc9184fb8921ea3
徐佳慧 <2649387037@qq.com>
2018-05-12 15:17:56 +0800

    CONTACT US栏目

--- SCIE/src/views/AboutUs/contactUs.vue
+++ SCIE/src/views/AboutUs/contactUs.vue
@@ -1,13 +1,32 @@
 <template>
-  <div>33</div>
+  <div>
+    <div v-html="content"></div>
+  </div>
 </template>
 <script>
+import { getAboutUsPage } from 'api/api';
 export default {
   data(){
-      return{}
+      return{
+          content:''
+      }
+  },
+  mounted(){
+      let that = this;
+      getAboutUsPage({name:'contactUs'}).then(res => {
+          console.log(res.data);
+          if (!res.data.code) {
+            that.content = res.data.content;
+          } else {
+              this.$message({
+                type: 'error',
+                message: res.data.message
+              })
+          } 
+      })
   }
 }
 </script>
-<style scoped>
+<style >
 
 </style>
