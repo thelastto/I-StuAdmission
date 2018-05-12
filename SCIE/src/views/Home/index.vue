b79bd5ea130c8f5eca8292bfe199117f95d943bc
徐佳慧 <2649387037@qq.com>
2018-05-12 15:18:51 +0800

    HOME栏目

--- SCIE/src/views/Home/index.vue
+++ SCIE/src/views/Home/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    111111111111111111111111111
+    <el-carousel indicator-position="outside">
+    <el-carousel-item v-for="item in newsList" :key="item">
+      
+    </el-carousel-item>
+  </el-carousel>
   </div>
   
 </template>
@@ -11,6 +15,20 @@ export default {
   }
 }
 </script>
-<style scoped>
-
+<style>
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 300px;
+    margin: 0;
+  }
+  
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
 </style>
